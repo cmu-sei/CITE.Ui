86f1750ef18c4268288e58a86baa3a389b3f55e8
sei-tspencer <trspencer@sei.cmu.edu>
2024-10-03 17:12:25 -0400

    build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // Copyright 2022 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license,
 // please see LICENSE.md in the project root for license information or contact permission@sei.cmu.edu for full terms.
-
+declare var require: any;
 export const environment = {
   production: true,
   VERSION: require('../../package.json').version,
